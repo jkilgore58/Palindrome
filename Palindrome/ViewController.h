c2879c1abcd88bebf842b84ade29960b1d700313
Jonathan Kilgore <jonathan.kilgore58@gmail.com>
2016-01-13 10:23:18 -0600

    Initial Commit

---
+++ Palindrome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Palindrome
+//
+//  Created by Jonathan Kilgore on 1/13/16.
+//  Copyright © 2016 Jonathan Kilgore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Palindrome/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Palindrome
+//
+//  Created by Jonathan Kilgore on 1/13/16.
+//  Copyright © 2016 Jonathan Kilgore. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
