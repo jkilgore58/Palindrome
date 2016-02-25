11b23496ea33bf4a3adaf0dd806e5473ced4251e
Jonathan Kilgore <jonathan.kilgore58@gmail.com>
2016-02-25 15:08:16 -0600

    updated with working code

--- Palindrome/ViewController.m
+++ Palindrome/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -16,12 +18,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)onCheckTapped:(UIButton *)sender {
+    
+    NSString *palinedrome = self.textField.text;
+    NSUInteger length = palinedrome.length;
+    NSLog(@"palinedrome length == %lu", length);
+    
+    for (int i = 0; i < length / 2; i++) {
+        NSLog(@"palinedrome [%i] == %c", i, [palinedrome characterAtIndex:i]);
+        if ([palinedrome characterAtIndex:i] != [palinedrome characterAtIndex:length - i - 1]) {
+            self.label.text = @"Not a palinedrome.";
+            return;
+        }
+        
+    }
+    NSLog(@"Done.");
+    self.label.text = @"It is a palinedrome.";
+}
+
+- (IBAction)onMakeTapped:(UIButton *)sender {
+    NSString *firstPart = self.textField.text;
+    NSMutableString *secondPart = [NSMutableString new];
+    NSString *result = @"";
+    
+//    for (int i = firstPart.length - 1; i >= 0; i--) {
+//        [secondPart appendString:[firstPart characterAtIndex:i]];
+//    }
+    
+    
+//    for (NSUInteger i = firstPart.length; i >= 0; i--) {
+//        NSUInteger index = i -1;
+//        
+//        unichar c = [firstPart characterAtIndex:index];
+//        result = [result stringByAppendingFormat:@"%C", c];
+//    
+//    //return;
+//    }
+    
+    self.label.text = [NSString stringWithFormat:@"%@%@", firstPart, result];
 }
 
 @end
